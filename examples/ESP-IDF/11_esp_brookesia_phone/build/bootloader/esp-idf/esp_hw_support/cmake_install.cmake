b8be12b101cf3c5bc6521e11e21d0f5bff17f159
MuddyPaws2 <john.tomlinson@cox.net>
2026-06-28 18:37:00 -0500

    Updated Video Driver

--- examples/ESP-IDF/11_esp_brookesia_phone/build/bootloader/esp-idf/esp_hw_support/cmake_install.cmake
+++ examples/ESP-IDF/11_esp_brookesia_phone/build/bootloader/esp-idf/esp_hw_support/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/John/.espressif/tools/riscv32-esp-elf/esp-15.2.0_20251204/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Espressif/tools/riscv32-esp-elf/esp-15.2.0_20251204/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
